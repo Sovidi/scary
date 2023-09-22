5729081f17bb9824b768cfa5566b52695be2412b
wjdalswjdals <roaring_frog@zohomail.com>
2023-09-22 16:26:09 +0900

    8

--- src/Component/Ghost.js
+++ src/Component/Ghost.js
@@ -1,24 +1,32 @@
 import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { myContext } from '../Context';
+import useSound from 'use-sound';
+import soundFile from "../media/scream.mp3";
 
 
 function Ghost() {
   const navigate = useNavigate();
+  const [play, { stop }] = useSound(soundFile, { loop: true, autoplay: true });
 
   function getRandomNumber() {
     return Math.floor(Math.random() * 4) + 1;
-}
+  }
   const randomNum = getRandomNumber();
 
   useEffect(()=>{
-    getRandomNumber()
+    getRandomNumber();
     setTimeout(() => {
       navigate("/")
     }, 1000);
-  }, [])
+  }, []);
 
-
+  useEffect(()=>{
+    play();
+    return () => {
+      stop();
+    };
+  }, [play, stop]);
 
   return (
       <section onClick={()=>{navigate("/")}} className='ghostPop'>
