0d5daa5dc54665f1fce8457958dcd1a5e5ed803f
Sovidi <lim132445@gmail.com>
2024-05-08 03:29:13 +0900

    pw

--- src/Component/EndRoom.js
+++ src/Component/EndRoom.js
@@ -1,55 +1,118 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { myContext } from '../Context';
 
 function EndRoom() {
-    const {data, fetchFn} = useContext(myContext);
-    const navigate = useNavigate();
-    const elForm = useRef();
+  const { data, fetchFn, dispatch } = useContext(myContext);
+  const elForm = useRef();
+  const mdForm = useRef();
+  const [mode, setMode] = useState("write");
+  const [modObj, setModObj] = useState([{name: "", message: ""}]);
+  const [chkObj, setChkObj] = useState(false);
+  const modName = useRef("");
+  const modMessage = useRef("");
+  const modPW = useRef("");
 
-    const insert = (e) => {
-      e.preventDefault()      
-      let formData = new FormData(e.target);
-      let today = new Date();
-      let date = `${today.getFullYear()}.${today.getMonth()+1}.${today.getDate()}`
-      formData.append("date", date);
-      let objData = Object.fromEntries(formData);
-      fetchFn("comment", objData);
-      e.target.name.value = "";
-      e.target.message.value = "";
-    }
+  const insert = async (e) => {
+    e.preventDefault()
+    let formData = new FormData(e.target);
+    let today = new Date();
+    let date = `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}`
+    formData.append("date", date);
+    formData.append("key", Date.now());
+    let objData = Object.fromEntries(formData);
+    await fetchFn("comment", objData);
+    e.target.name.value = "";
+    e.target.message.value = "";
+  };
 
-    useEffect(()=>{
-      fetchFn("commentGet", "");
-    }, [])
-    
-    if(!data) console.log(data);
-    
+  const modify = async () => {
+    await fetchFn("update", modObj[0]);
+    modPW.current.value = "";
+    setMode("write");
+  };
+
+  const commentDelete = async () => {
+    await fetchFn("delete", modObj[0]);
+    modPW.current.value = "";
+    setMode("write");
+  };
+
+  const modPrepare = async (e, item) => {
+    e.preventDefault();
+    const modData = data.filter(obj => obj.key == item.key);
+    setModObj(modData);
+    setChkObj(item);
+    modPW.current.value = "";
+    setMode("modify");
+  };
+
+
+
+  useEffect(()=>{
+    // modName.current.value = modObj[0].name;
+    // modMessage.current.value = modObj[0].message;
+    // modPW.current.value = "";
+    console.log(modObj);
+  }, [modObj]);
+
+  // useEffect(()=>{
+    // setChkObj(data.filter(item=>{
+    //   return modObj.some(obj => obj.name == item.name && obj.password == item.password && obj.message == item.message)
+    // }))
+  //   if(chkObj) {
+  //     alert("수정 오류 발생");
+  //   };
+  // }, [data]);
+
+  useEffect(() => {
+    fetchFn();
+  }, []);
+
+  if (!data) return <>로딩중</>;
+
   return (
     <section className='comment'>
       <p>추카추카 탈출하셨습니다.</p>
       <p>방명록을 남겨주세요</p>
-      <form onSubmit={(e)=>{insert(e)}} className='text' ref={elForm}>
-          <input required type='text' placeholder='이름' name='name'/>
-          <textarea required placeholder='댓글' name='message'></textarea>
-          <button>등록하기</button>
-          <button type="button" onClick={()=>{fetchFn("update", "")}}>수정실험</button>
+      <form onSubmit={(e) => { insert(e) }} className={`text ${mode == "write" ? "active" : ""}`} ref={elForm}>
+        <input required type='text' placeholder='이름' name='name' />
+        <input required type='password' placeholder='비밀번호' name='password' />
+        <textarea required placeholder='댓글' name='message'></textarea>
+        <button>등록하기</button>
       </form>
-
-      <p>방명록 확인</p>
-      <ul className='commentList'>
-        {
-          data.map(item=>(
-            <li className='commentBox'>
-              <div>
-                <p>{item.name}</p>
-                <p>{item.date}</p>
-              </div>
-              <p>{item.message}</p>
-            </li>
-          ))
-        }
-      </ul>
+      <form onSubmit={(e) => { modify(e) }} className={`text ${mode == "modify" ? "active" : ""}`} ref={mdForm}>
+        <input required type='text' placeholder='이름' name='name' value={modObj[0].name} onChange={(e) => { setModObj(modObj.map(item => { item.name = e.target.value; return item; })) }} />
+        <input ref={modPW} required type='password' placeholder='비밀번호' name='password' onChange={(e)=>{ setModObj(modObj.map(item => {item.password = e.target.value; return item;}))}} />
+        <textarea required placeholder='수정할 내용' name='message' value={modObj[0].message} onChange={(e) => { setModObj(modObj.map(item => { item.message = e.target.value; return item; })) }}></textarea>
+        <button type='button' onClick={()=>{ modify(); }}>수정하기</button>
+        <button type='button' onClick={()=>{ commentDelete(); }}>삭제하기</button>
+        <button type='button' onClick={async () => { setMode("write"); fetchFn(); }}>취소하기</button>
+      </form>
+      {/* <form onSubmit={(e) => { modify(e) }} className={`text ${mode == "modify" ? "active" : ""}`} ref={mdForm}>
+        <input ref={modName} required type='text' placeholder='이름' name='name'/>
+        <input ref={modPW} required type='password' placeholder='비밀번호' name='password' />
+        <textarea ref={modMessage} required placeholder='수정할 내용' name='message' />
+        <button>수정하기</button>
+        <button type='button' onClick={}>삭제하기</button>
+        <button type='button' onClick={() => { setMode("write"); }}>취소하기</button>
+      </form> */}
+      <div className={`commentBox ${mode == "write" ? "" : "active"}`}>
+        <p>방명록 확인</p>
+        <ul className='commentList'>
+          {
+            data.map(item => (
+              <li className='comments'>
+                <div>
+                  <p>{item.name}</p>
+                  <p>{item.date}</p>
+                </div>
+                <p>{item.message}</p>
+                <button type="button" onClick={(e) => { modPrepare(e, item) }}>삭제/수정하기</button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </section>
   )
 }
--- src/Context.js
+++ src/Context.js
@@ -1,113 +1,110 @@
 import React, { useEffect, useReducer } from 'react'
 import { createContext } from 'react'
 import axios from "axios"
-import { useParams } from 'react-router-dom';
 
 export const myContext = createContext();
 
 const insert = (state, action) => {
-    switch(action.type) {
-        case "space" : return action.d;
-        // case "search" : return action.d;
-        case "comment" : return action.d;
-        case "commentGet" : return action.d;
-        default : return action.d;
-    }
+	switch (action.type) {
+		case "space": return action.d;
+		case "comment": return action.d;
+		case "commentGet": return action.d;
+		case "update": return action.d;
+		case "delete": return action.d;
+		// case "search" : return action.d;
+		default: return action.d;
+	}
 }
 
-function Context({children}) {
-    const [data, dispatch] = useReducer(insert, []);
+function Context({ children }) {
+	const [data, dispatch] = useReducer(insert, []);
 
-    function getRandomNumber1() {
-        return Math.floor(Math.random() * 3) + 1;
-    }
-    const randomNum1 = getRandomNumber1();
-    
-    function getRandomNumber2() {
-        return Math.floor(Math.random() * 3) + 1;
-    }
-    const randomNum2 = getRandomNumber2();
-    
-    function getRandomNumber3() {
-        return Math.floor(Math.random() * 3) + 1;
-    }
-    const randomNum3 = getRandomNumber3();
+	function getRandomNumber1() {
+		return Math.floor(Math.random() * 3) + 1;
+	}
+	const randomNum1 = getRandomNumber1();
+
+	function getRandomNumber2() {
+		return Math.floor(Math.random() * 3) + 1;
+	}
+	const randomNum2 = getRandomNumber2();
+
+	function getRandomNumber3() {
+		return Math.floor(Math.random() * 3) + 1;
+	}
+	const randomNum3 = getRandomNumber3();
 
 
-    function getRandomDate() {
-        const startYear = 2010;
-        const endYear = 2020;
-        const year = Math.floor(Math.random() * (endYear - startYear + 1)) + startYear;
-        const month = (Math.floor(Math.random() * 12) + 1).toString().padStart(2, '0');
-        const daysInMonth = new Date(year, month, 0).getDate();
-        const day = (Math.floor(Math.random() * daysInMonth) + 1).toString().padStart(2, '0');
-    
-        const date = `${year}-${month}-${day}`;
-        return date;
-    }    
-    const randomDate = getRandomDate();
+	function getRandomDate() {
+		const startYear = 2010;
+		const endYear = 2020;
+		const year = Math.floor(Math.random() * (endYear - startYear + 1)) + startYear;
+		const month = (Math.floor(Math.random() * 12) + 1).toString().padStart(2, '0');
+		const daysInMonth = new Date(year, month, 0).getDate();
+		const day = (Math.floor(Math.random() * daysInMonth) + 1).toString().padStart(2, '0');
 
-    const server = axios.create({
-        baseURL:"https://port-0-scary-server-6w1j2alm48bfok.sel5.cloudtype.app"
-        // baseURL:"http://localhost:3050/"
-    })
+		const date = `${year}-${month}-${day}`;
+		return date;
+	}
+	const randomDate = getRandomDate();
 
-    const space = axios.create({
-        baseURL:"https://api.nasa.gov/planetary/apod",
-        params: {
-            api_key: "yq2fKjbfLH7bFiRl80TMckGQxLFsQUhpleuxOUCD",
-            date : `${randomDate}`
-        }
-    })
+	const server = axios.create({
+		// baseURL:"https://port-0-scary-server-6w1j2alm48bfok.sel5.cloudtype.app"
+		baseURL: "http://localhost:3050/"
+	})
 
-    // const searchTest = axios.create({
-    //     baseURL:"https://cse.google.com/cse.js",
-    //     params: {
-    //         api_key:"AIzaSyBwsbJu8-7pvUtXs03dfYmwO4nZDPiLTJA",
-    //         cx: "53ed9f2feaec14c77"
-    //     }
-    // })
+	// const space = axios.create({
+	// 	baseURL: "https://api.nasa.gov/planetary/apod",
+	// 	params: {
+	// 		api_key: "yq2fKjbfLH7bFiRl80TMckGQxLFsQUhpleuxOUCD",
+	// 		date: `${randomDate}`
+	// 	}
+	// })
 
-    // if(!data) return <>로딩중</>
-    const fetchFn = async (type, data) => {
-        let res;
-        switch (type) {
-            case "space" :
-                res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=yq2fKjbfLH7bFiRl80TMckGQxLFsQUhpleuxOUCD&date=${randomDate}`);
-                res = res.data;
-                break;
-            case "commentGet" :
-                res = await server.get("/abc");
-                res = res.data;
-                break;
-            case "comment" :
-                res = await server.post("/insert", data);
-                res = res.data;
-                break;
-            case "update" :
-                res = await server.put(`/update`, {msg: "아힝"});
-                res = res.data;
-                console.log(res);
-            // case "search" : 
-            //     res = await searchTest.get(`/`, {params: {q: data}});
-            //     break;
-            default :                 
-                res = await server.get("/abc");
-                res = res.data;
-        }
-        dispatch({type, d: res});
-        // console.log(res.data);
-    }
+	// const searchTest = axios.create({
+	//     baseURL:"https://cse.google.com/cse.js",
+	//     params: {
+	//         api_key:"AIzaSyBwsbJu8-7pvUtXs03dfYmwO4nZDPiLTJA",
+	//         cx: "53ed9f2feaec14c77"
+	//     }
+	// })
 
-    useEffect(()=>{
-        // fetchFn("commentGet", "");
-    }, []);
+	const fetchFn = async (type, data) => {
+		let res;
+		switch (type) {
+			case "space":
+				res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=yq2fKjbfLH7bFiRl80TMckGQxLFsQUhpleuxOUCD&date=${randomDate}`);
+				res = res.data;
+				break;
+			case "commentGet":
+				res = await server.get("/select");
+				res = res.data;
+				break;
+			case "comment":
+				res = await server.post("/insert", data);
+				res = res.data;
+				break;
+			case "update":
+				res = await server.put(`/update`, data);
+				res = res.data;
+				break;
+			case "delete" : 
+				res = await server.delete(`/delete?item=${JSON.stringify(data)}`);
+				res = res.data;
+				break;
+			default:
+				res = await server.get("/select");
+				res = res.data;
+		}
+		console.log(res);
+		dispatch({ type, d: res });
+	}
 
-    return (
-        <myContext.Provider value={{data, fetchFn, randomNum1, randomNum2, randomNum3, getRandomNumber1, getRandomNumber2, getRandomNumber3}}>
-            {children}
-        </myContext.Provider>
-    )
+	return (
+		<myContext.Provider value={{ data, fetchFn, randomNum1, randomNum2, randomNum3, getRandomNumber1, getRandomNumber2, getRandomNumber3 }}>
+			{children}
+		</myContext.Provider>
+	)
 }
 
 export default Context
